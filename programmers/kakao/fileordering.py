e8e4f61167e5246df140008e2c96b3c2e8e17023
bcnote3314 <wogus3314@gmail.com>
2021-08-28 21:30:11 +0000

    fileordering udate code

--- programmers/kakao/fileordering.py
+++ programmers/kakao/fileordering.py
@@ -31,11 +31,18 @@ def solution(files):
 
     return answer
 
-            
+import re
+
+def solution2(files):
+    a = sorted(files, key=lambda file : int(re.findall('\d+', file)[0]))
+    b = sorted(a, key=lambda file : re.split('\d+', file.lower())[0])
+    return b
 
 if __name__ == '__main__':
     print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
     print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
+    print(solution2(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
+    print(solution2(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
 
 
     
