faa485775b6b87a137b0e1beaf6953eb82d42f49
bcnote3314 <wogus3314@gmail.com>
2021-11-21 15:22:26 +0000

    lifeboat(greedy)

---
+++ programmers/base/greedy/lifeboat.py
@@ -0,0 +1,17 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42885
+
+def solution(people, limit):
+    answer = 0
+
+    people.sort()
+
+    i = 0
+    j = len(people) - 1
+
+    while i <= j:
+        answer += 1
+        if people[i] + people[j] <= limit:
+            i += 1
+        j -= 1
+
+    return answer
